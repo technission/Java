a30dd8eac7ea1dee35bd4a09d89d875ba82b55a2
Steve Thornton <technission@outlook.com>
2023-10-08 23:50:48 -0500

    Created MVC demo

---
+++ mvc_project/src/mvc_project/MVCPatternDemo.java
@@ -0,0 +1,29 @@
+package mvc_project;
+
+public class MVCPatternDemo {
+
+	public static void main(String[] args) {
+		// fetch student record based on his roll no from the database
+		Student model = retriveStudentFromDatabase();
+		
+		//Crate a view : to write student details on console
+		StudentView view = new StudentView();
+		
+		StudentController controller = new StudentController(model, view);
+		
+		controller.updateView();
+		
+		//update model data
+		controller.setStudentName("John");
+		controller.setStudentRollNo("15");
+		controller.updateView();
+
+	}
+
+	private static Student retriveStudentFromDatabase() {
+		Student student = new Student();
+		student.setName("Robert");
+		student.setRollNo("10");
+		return student;
+	}
+}
